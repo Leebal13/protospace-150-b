180ca18a9b6d2b345f8dfd26ff48cc7df3d122cc
pask3k <0304london.11boston.13@gmail.com>
2023-06-26 07:52:32 +0900

    Rubocopをかける
    bundle exec rubocop -a

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -8,7 +8,7 @@ class PrototypesController < ApplicationController
   def new
     @prototype = Prototype.new
   end
-  
+
   def create
     prototype = Prototype.new(prototype_params)
     if prototype.save
@@ -16,10 +16,8 @@ class PrototypesController < ApplicationController
     else
       @prototype = prototype
       render :new, status: :unprocessable_entity
-    end 
+    end
   end
-
- 
 
   private
 
@@ -27,4 +25,3 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
- 
