64d66963d018570208f744c1b730c02734763bc8
pask3k <0304london.11boston.13@gmail.com>
2023-06-26 15:04:14 +0900

    prototypes_controller.rbの重複記載を整理

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_prototype, only: [:show, :edit, :update]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -21,23 +22,15 @@ class PrototypesController < ApplicationController
   end
   
   def show
-    @prototype = Prototype.find(params[:id])
   end
   
   def edit
-    @prototype = Prototype.find(params[:id])
     if @prototype.user_id != current_user.id
       redirect_to action: :index
     end
   end
-
-  def show
-
-
-  end
  
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to action: :show
     else
@@ -54,13 +47,12 @@ class PrototypesController < ApplicationController
       redirect_to root_path
     end
   end
-  
+
   private
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-
 
   def move_to_index
     unless user_signed_in?
@@ -68,5 +60,7 @@ class PrototypesController < ApplicationController
     end
   end
 
-
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
 end
