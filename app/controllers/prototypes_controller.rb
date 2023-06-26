3fe4682a10d75c9eab01dccf79c46fc21f40d494
81168Tli <135498013+81168Tli@users.noreply.github.com>
2023-06-26 14:45:35 +0900

    仮）コメント投稿機能実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,6 @@
 class PrototypesController < ApplicationController
+
+
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -17,6 +19,10 @@ class PrototypesController < ApplicationController
     end 
   end
 
+  def show
+
+
+  end
  
 
   private
@@ -24,5 +30,11 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+
 end
  
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
-  resources :prototypes
+  resources :prototypes do
+    resources :comments, only: :create
+
+  end
   
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
